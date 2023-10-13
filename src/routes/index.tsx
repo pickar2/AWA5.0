838f43a24956e6ea33bd05d07e56f1a1cd8ea8d2
pickar <sereges2@gmail.com>
2023-10-14 02:54:51 +0300

    fix image

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
+import type { RunInfo } from "@/components/AWA5.0Interpreter";
 import {
-  RunInfo,
   State,
   runCode,
   awatismsToAwa,
@@ -8,14 +8,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import type { Accessor, JSX, JSXElement, Setter } from "solid-js";
-import {
-  createSignal,
-  onCleanup,
-  onMount,
-  Show,
-  type VoidComponent,
-} from "solid-js";
-import { Title } from "solid-start";
+import { createSignal, onCleanup, onMount, type VoidComponent } from "solid-js";
 
 const TextArea = (
   props: JSX.TextareaHTMLAttributes<HTMLTextAreaElement>
@@ -101,131 +94,134 @@ const Home: VoidComponent = () => {
   };
 
   return (
-    <main class="bg-slate-900 min-h-screen">
-      <div class="flex absolute justify-center items-center w-full h-full opacity-30 pointer-events-none">
-        <div class="relative">
-          <div class="absolute w-[33%] h-[17%] top-[8%] right-[27%] rounded-full rotate-[20deg] cursor-[grabbing] pointer-events-auto" />
-          <img
-            class="pointer-events-none select-none"
-            src="/jellyAwa.png"
-            alt="Jelly art by Sena Bonbon"
-          />
-        </div>
-      </div>
-      <div class="flex absolute top-4 right-4 flex-col gap-4">
-        <TextArea
-          ref={awatismInputRef}
-          class="w-[500px] resize-y"
-          name="awatism-input"
-          id="awatism-input"
-          rows={10}
-          onchange={updateTranslation}
-          onkeyup={updateTranslation}
-        />
-      </div>
-      <div class="flex flex-col gap-4 p-4">
-        <div class="flex gap-2">
-          <TextArea
-            ref={userInputRef}
-            class="w-[500px]"
-            name="user-input"
-            id="user-input"
-            disabled={!waitingForInput()}
-          />
-          <Button
-            variant={"outline"}
-            disabled={!waitingForInput()}
-            onClick={() => {
-              setWaitingForInput(false);
-            }}
-            ref={inputButtonRef}
-          >
-            Input
-          </Button>
-        </div>
-        <div class="flex gap-2">
-          <TextArea
-            ref={awaInputRef}
-            class="w-[500px]"
-            name="awa-input"
-            id="awa-input"
-            disabled={runInfo().state == State.Running}
-            onchange={updateDecompilation}
-            onkeyup={updateDecompilation}
-            value={"awa"}
-            placeholder="awa..."
-            rows={10}
-          />
-          <div class="flex flex-col gap-2 justify-center">
-            <RunState info={runInfo} setState={setRunInfo} />
-            <Button
-              variant={"outline"}
-              disabled={runInfo().state != State.Stopped}
-              onclick={() => {
-                if (!awaInputRef || !userInputRef || !outputRef) return;
-                setRunInfo({
-                  state: State.Running,
-                  startTime: Date.now(),
-                  timeMillis: 0,
-                });
-
-                outputRef.value = "";
-
-                const writeToOutput = (str: string) => {
-                  if (!outputRef) return;
-                  outputRef.value += str;
-                  outputRef.scrollTop = outputRef.scrollHeight;
-                };
-
-                const getInput = async (): Promise<string> => {
-                  setWaitingForInput(true);
-                  setRunInfo({ ...runInfo(), state: State.Paused });
-                  return new Promise((resolve) => {
-                    lastResolve = resolve;
-                    inputButtonRef!.addEventListener(
-                      "click",
-                      () => {
-                        setWaitingForInput(false);
-                        setRunInfo({ ...runInfo(), state: State.Running });
-                        lastResolve = undefined;
-                        resolve(userInputRef!.value);
-                      },
-                      { once: true }
-                    );
-                  });
-                };
-
-                void runCode(
-                  awaInputRef!.value,
-                  writeToOutput,
-                  getInput,
-                  runInfo,
-                  setRunInfo
-                );
-              }}
-            >
-              Run
-            </Button>
-            <Button
-              variant={"outline"}
-              disabled={runInfo().state == State.Stopped}
-              onclick={stopExecution}
-            >
-              Stop
-            </Button>
+    <>
+      <main class="bg-slate-900 min-h-screen">
+        <div class="flex absolute justify-center items-center w-full h-full opacity-30 pointer-events-none">
+          <div class="relative">
+            <div class="absolute w-[33%] h-[17%] top-[8%] right-[27%] rounded-full rotate-[20deg] cursor-[grabbing] pointer-events-auto" />
+            <img
+              class="pointer-events-none select-none"
+              src="/AWA5.0/jellyAwa.png"
+              alt="Jelly art by Sena Bonbon"
+            />
           </div>
         </div>
-        <div class="flex gap-2">
+        <div class="flex absolute top-4 right-4 flex-col gap-4">
           <TextArea
-            ref={outputRef}
-            class="w-[500px]"
-            name="output"
-            id="output"
-            disabled={runInfo().state == State.Running}
+            ref={awatismInputRef}
+            class="w-[500px] resize-y"
+            name="awatism-input"
+            id="awatism-input"
+            rows={10}
+            onchange={updateTranslation}
+            onkeyup={updateTranslation}
           />
         </div>
-      </div>
-    </main>
+        <div class="flex flex-col gap-4 p-4">
+          <div class="flex gap-2">
+            <TextArea
+              ref={userInputRef}
+              class="w-[500px]"
+              name="user-input"
+              id="user-input"
+              disabled={!waitingForInput()}
+            />
+            <Button
+              variant={"outline"}
+              disabled={!waitingForInput()}
+              onClick={() => {
+                setWaitingForInput(false);
+              }}
+              ref={inputButtonRef}
+            >
+              Input
+            </Button>
+          </div>
+          <div class="flex gap-2">
+            <TextArea
+              ref={awaInputRef}
+              class="w-[500px]"
+              name="awa-input"
+              id="awa-input"
+              disabled={runInfo().state == State.Running}
+              onchange={updateDecompilation}
+              onkeyup={updateDecompilation}
+              value={"awa"}
+              placeholder="awa..."
+              rows={10}
+            />
+            <div class="flex flex-col gap-2 justify-center">
+              <RunState info={runInfo} setState={setRunInfo} />
+              <Button
+                variant={"outline"}
+                disabled={runInfo().state != State.Stopped}
+                onclick={() => {
+                  if (!awaInputRef || !userInputRef || !outputRef) return;
+                  setRunInfo({
+                    state: State.Running,
+                    startTime: Date.now(),
+                    timeMillis: 0,
+                  });
+
+                  outputRef.value = "";
+
+                  const writeToOutput = (str: string) => {
+                    if (!outputRef) return;
+                    outputRef.value += str;
+                    outputRef.scrollTop = outputRef.scrollHeight;
+                  };
+
+                  const getInput = async (): Promise<string> => {
+                    setWaitingForInput(true);
+                    setRunInfo({ ...runInfo(), state: State.Paused });
+                    return new Promise((resolve) => {
+                      lastResolve = resolve;
+                      inputButtonRef!.addEventListener(
+                        "click",
+                        () => {
+                          setWaitingForInput(false);
+                          setRunInfo({ ...runInfo(), state: State.Running });
+                          lastResolve = undefined;
+                          resolve(userInputRef!.value);
+                        },
+                        { once: true }
+                      );
+                    });
+                  };
+
+                  void runCode(
+                    awaInputRef!.value,
+                    writeToOutput,
+                    getInput,
+                    runInfo,
+                    setRunInfo
+                  );
+                }}
+              >
+                Run
+              </Button>
+              <Button
+                variant={"outline"}
+                disabled={runInfo().state == State.Stopped}
+                onclick={stopExecution}
+              >
+                Stop
+              </Button>
+            </div>
+          </div>
+          <div class="flex gap-2">
+            <TextArea
+              ref={outputRef}
+              class="w-[500px]"
+              name="output"
+              id="output"
+              disabled={runInfo().state == State.Running}
+            />
+          </div>
+        </div>
+        <div class="absolute bottom-1 left-1">Awa5.0 Logo by Sena Bonbon</div>
+      </main>
+    </>
   );
 };
 
